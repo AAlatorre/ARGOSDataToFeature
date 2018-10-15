2933b876f846fba1304798c976c43b188ada2a63
AAlatorre <42745677+AAlatorre@users.noreply.github.com>
2018-10-15 12:16:23 -0400

    Modules and input variables

---
+++ ImportARGOS.py
@@ -0,0 +1,18 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a point
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2018
+## Author: Andrea Alatorre - aa449@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
+
+# Set input variables (Hard-wired)
+inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
